39553e04f082ca9baec1d97eba23548896fcec0a
Felipe Ishimine <felipeishimine@gmail.com>
2026-08-27 11:07:41 -0300

    Asset bundle builder

---
+++ Editor/AssetBundleBuilder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+
+public static class AssetBundleBuilder
+{
+	[MenuItem("Tools/Build AssetBundles")]
+	static void Build()
+	{
+		BuildPipeline.BuildAssetBundles(
+			"AssetBundles",
+			BuildAssetBundleOptions.ChunkBasedCompression,
+			BuildTarget.WebGL
+		);
+	}
+}
